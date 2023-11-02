f7b94567b4492bd5f4178434e09403581b50c399
Leah Chung <102619868+Captain1014@users.noreply.github.com>
2023-11-02 14:53:46 -0400

    Update Array_Hashing.py

--- Array_Hashing.py
+++ Array_Hashing.py
@@ -13,12 +13,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-      hset = set()
-      for num in nums:
-        if num in hset:
-          return True
+      hset = set()   # make a hash set to compare
+      for num in nums: 
+        if num in hset: # is this num in the hset?
+          return True # yes, return true
         else:
-          hset.add(num)
+          hset.add(num) # no, add it to the hset and repeat this
           
 # Valid Anagram solution 1 using hashSet
 class Solution(object):
@@ -28,20 +28,22 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t) :
+        if len(s) != len(t) : # the lengths are different. fail
             return False
 
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
+        countS, countT = {}, {} # make two hash sets to compare
+        for i in range(len(s)): # len(s) or len(t) doesn't matter at this point
+            countS[s[i]] = 1 + countS.get(s[i],0) # countS[s[i]] starts at 0, and increases as we loop over s[i] 
+                                                # get(s[i],0) is looking for s[i] char, if no exist, return 0. if exists, return the current count of s[i]
+                                                # 1 + is to increment the count of s[i]
             countT[t[i]] = 1 + countT.get(t[i],0)
         for c in countS:
-            if countS[c] != countT.get(c,0):
+            if countS[c] != countT.get(c,0):  # compare the counts of the char in each set
                 return False
         return True
 
   
-  # why do we use get?
+
 
 # solution 2 using sort
 class Solution:
